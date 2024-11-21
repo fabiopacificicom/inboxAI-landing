a5bd7fa91f97c5fe2325867865d3f4104793b262
fab <mail@fabiopacifici.com>
2024-11-21 23:57:53 +0100

    minor adjustments

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -13,12 +13,21 @@ export default function Home() {
             capabilities to your email inbox. As we're still in the early stages of development (alpha), this is a chance for you to join us on
             our journey and contribute to shaping the future of email management.
 
-            **What can you expect from our documentation?**
+
+          </p>
+
+          <h4 className="mt-4">
+            What can you expect from our documentation?
+          </h4>
+          <p>
 
             Our documentation page provides an overview of the project, including installation guides, setup tutorials, and feature explanations.
             We'll also be sharing updates on our development progress, known issues, and plans for future releases.
 
-            **Before diving in...**
+          </p>
+          <h4 className="mt-4"> Before diving in...</h4>
+          <p>
+
 
             Please note that this is an early alpha release, which means you may encounter bugs or unfinished features. If you're not comfortable
             with experimenting with beta software, we recommend waiting until we reach a more stable milestone.
@@ -26,12 +35,13 @@ export default function Home() {
             That being said, if you're up for the challenge and want to be part of shaping the future of Inbox AI, feel free to explore our
             documentation further. Click the link below to access our README file, which contains all the details you need to get started.
 
-            <a href="https://github.com/fabiopacificicom/inboxAI/blob/main/README.md" className="d-flex gap-2" target="_blank">
-              <i className="bi bi-github"></i>
-              This is the documentation page
-              <i className="bi bi-arrow-right-square" ></i>
-            </a>
+
           </p>
+          <a href="https://github.com/fabiopacificicom/inboxAI/blob/main/README.md" className="btn d-flex gap-2" target="_blank">
+            <i className="bi bi-github"></i>
+            This is the documentation page
+            <i className="bi bi-arrow-right-square" ></i>
+          </a>
         </div>
       </div>
     </main>

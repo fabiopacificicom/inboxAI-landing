d639f84cc2937ae5c6b2d20ef35c57b67078cd34
Fabio Pacifici <72492598+fabiopacifici@users.noreply.github.com>
2024-11-22 00:35:42 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,8 +18,8 @@ export default function Footer() {
           <div className="col offset-md-4">
             <h3 className='my-3'>Quick Links</h3>
             <ul style={{ listStyle: 'none' }}>
-              <li><i className="bi bi-link"></i><Link to="/"> Home </Link></li>
-              <li><i className="bi bi-link"></i><Link to="/docs"> Documentation </Link></li>
+              <li><i className="bi bi-link"></i> <Link to="/">Home </Link></li>
+              <li><i className="bi bi-link"></i> <Link to="/docs">Documentation </Link></li>
 
               <li><a target='_blank' href="https://github.com/fabiopacificicom/inboxAI"><i className="bi bi-link"></i> Source Code</a></li>
               <li><a target='_blank' href="https://github.com/fabiopacificicom/inboxAI/issues"><i className="bi bi-link"></i> Report a bug</a></li>
